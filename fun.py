1bf8c14f86505aee45059b3703b95cbe48839f83
Asenath0 <60759090+Asenath0@users.noreply.github.com>
2020-03-10 21:57:29 +0100

    Update fun.py

--- fun.py
+++ fun.py
@@ -1,4 +1,5 @@
 import local
+import os
 
 #Funkcja sprawdza hasło i login użytkownika, ustawia który to użytkownik w pliku user.txt
 #w pliku zapis wygląda tak:
@@ -51,6 +52,8 @@ def add_student():
     login = input("Podaj login dla ucznia: ")
     password = input("Podaj haslo dla ucznia: ")
 
+
+    #logins and passwords
     logowanie.write(login)
     logowanie.write("\n")
     logowanie.write(password)
@@ -62,6 +65,8 @@ def add_student():
     logowanie.write("\n")
     logowanie.close()
 
+
+    #local
     source = open("local.py").readlines()
     goal = open("local.py", "w")
     for i in source:
@@ -76,3 +81,45 @@ def add_student():
     source.write(",")
     source.write("]")
     source.close()
+
+
+    #folder
+    def folder():
+        class_ = int(input("Podaj numer klasy (1 - 3): "))
+        if (class_ == 1):
+            klasa1_dir = '/klasy/1'
+
+            try:
+                path4 = os.path.join(klasa1_dir, name)
+                os.mkdir(path4)
+            except OSError:
+                print("Błąd przy tworzeniu podfolderu ucznia")
+            else:
+                pass
+
+        elif (class_ == 2):
+            klasa2_dir = '/klasy/2'
+
+            try:
+                path5 = os.path.join(klasa2_dir, name)
+                os.mkdir(path5)
+            except OSError:
+                print("Błąd przy tworzeniu podfolderu ucznia")
+            else:
+                pass
+
+        elif (class_ == 3):
+            klasa3_dir = '/klasy/3'
+
+            try:
+                path6 = os.path.join(klasa3_dir, name)
+                os.mkdir(path6)
+            except OSError:
+                print("Błąd przy tworzeniu podfolderu ucznia")
+            else:
+                pass
+
+        else:
+            print ("Nieprawidłowy numer klasy")
+            folder()
+    folder()
